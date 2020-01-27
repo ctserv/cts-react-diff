8444a72b7667a749d7d2ae4c6c2107f4bec959c8
Joe Salloum <joe.salloum@ctsjounieh.com>
2020-01-27 17:20:33 +0200

    react 16 version

---
+++ dist/react-diff.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var React = require('react');
+var CreateClass = require('create-react-class');
+var PropTypes = require('prop-types');
+var jsdiff = require('diff');
+
+var fnMap = {
+  'chars': jsdiff.diffChars,
+  'words': jsdiff.diffWords,
+  'sentences': jsdiff.diffSentences,
+  'json': jsdiff.diffJson
+};
+
+module.exports = CreateClass({
+  displayName: 'Diff',
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      inputA: '',
+      inputB: '',
+      type: 'chars'
+    };
+  },
+
+  propTypes: {
+    inputA: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    inputB: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    type: PropTypes.oneOf(['chars', 'words', 'sentences', 'json'])
+  },
+
+  render: function render() {
+    var diff = fnMap[this.props.type](this.props.inputA, this.props.inputB);
+    var result = diff.map(function (part, index) {
+      var spanStyle = {
+        backgroundColor: part.added ? 'lightgreen' : part.removed ? 'salmon' : 'lightgrey'
+      };
+      return React.createElement(
+        'span',
+        { key: index, style: spanStyle },
+        part.value
+      );
+    });
+    return React.createElement(
+      'pre',
+      { className: 'diff-result' },
+      result
+    );
+  }
+});
+
